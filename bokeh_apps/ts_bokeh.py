77f912fe1576ebd7ba4e1916023b03def64cba8a
LaurentBerger <laurent.berger@univ-lemans.fr>
2022-05-03 11:46:12 +0200

    latex

--- bokeh_apps/ts_bokeh.py
+++ bokeh_apps/ts_bokeh.py
@@ -1,12 +1,14 @@
-from django.views.decorators.csrf import csrf_protect
+import os
 import numpy as np
+from django.views.decorators.csrf import csrf_protect
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
+import django.contrib.staticfiles 
 
 from bokeh.io import curdoc
 from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, Slider
+from bokeh.models import ColumnDataSource, Slider, Toolbar
 from bokeh.plotting import figure
 from bokeh.document import Document
 from bokeh.embed import server_document
@@ -14,24 +16,76 @@ from bokeh.layouts import column, row
 from bokeh.models import Text, Arc, Line, ColumnDataSource, Slider
 from bokeh.plotting import figure
 from bokeh.embed import file_html, components
-from bokeh.models import CustomJS, RangeSlider
-from bokeh.models import BoxSelectTool, LassoSelectTool
+from bokeh.models import CustomJS, RangeSlider, ImageURL
+from bokeh.models import BoxSelectTool, LassoSelectTool, BoxZoomTool
 
 import traimaocv.models.cours_ts as ts_crs
-
+from bokeh.models import MathML
 #theme = Theme(filename=join(settings.THEMES_DIR, "theme.yaml"))
 
+DIR_DFT = os.path.dirname(django.contrib.staticfiles.finders.find("favicon.ico"))
+LATEX =r"""\documentclass[border=12pt,12pt,varwidth]{standalone}
+\usepackage{amssymb,amsmath}
+\usepackage[utf8]{inputenc}
+\begin{document}
+$$ equation $$
+\end{document}
+"""
+mathml = """
+<math>
+  <mrow>
+    <mfrac>
+      <mn>1</mn>
+      <mn>4</mn>
+    </mfrac>
+    <msup>
+      <mi>x</mi>
+      <mn>2</mn>
+    </msup>
+  </mrow>
+</math>
+"""
+
+def latex_url(theta):
+    
+    os.chdir(DIR_DFT)
+    idx_file = str(int(theta*100))
+    nom_fichier = "equation"+idx_file
+    if os.path.isfile(nom_fichier+".png"):
+        return "/static/equation"+idx_file+".png"
+    legende = LATEX.replace("equation",r"\boldsymbol{\theta = "+str(int(theta*100)/100)+"}")
+    fichier_ltx = open(os.path.join(DIR_DFT,nom_fichier+".tex"),"w",encoding="utf-8")
+    fichier_ltx.write(legende)
+    fichier_ltx.close()
+    cmd = "latex --shell-escape --quiet -output-directory="+DIR_DFT +" "+nom_fichier+".tex"
+    print(cmd)
+    os.system(cmd)
+    cmd = "dvipng -bg Transparent -D 1200 "+ \
+          os.path.join(DIR_DFT,nom_fichier + ".dvi") + \
+          " -o "+ nom_fichier +".png"
+    print(cmd)
+    os.system(cmd)
+    os.remove(os.path.join(DIR_DFT, nom_fichier+".tex"))
+    os.remove(os.path.join(DIR_DFT, nom_fichier+".aux"))
+    os.remove(os.path.join(DIR_DFT, nom_fichier+".dvi"))
+    os.remove(os.path.join(DIR_DFT, nom_fichier+".log"))
+   
+    return "/static/equation"+idx_file+".png"
 
 def cercle_trigo_bkh(request: HttpRequest) -> HttpResponse:
-    theta = np.pi/3
+    theta = 1
     b_ok, val = ts_crs.get_arg_post(request, ['theta'])
     if b_ok:
         theta = float(val[0])
-    plot = figure(width=400,
-                  height=400,
+    outils = Toolbar(tools=[BoxSelectTool(), LassoSelectTool(),BoxZoomTool()])
+    plot = figure(width=600,
+                  height=600,
                   title="Cercle trigonométrique",
                   name="Mes_donnees",
-                  match_aspect=True)
+                  match_aspect=True,
+                  tools="wheel_zoom, reset, save")
+    plot.yaxis.axis_label = MathML(text=mathml)
+     
     plot.circle(0, 0, radius=1,
                 fill_color=None,
                 line_color='OliveDrab',
@@ -42,10 +96,11 @@ def cercle_trigo_bkh(request: HttpRequest) -> HttpResponse:
     x = [0]
     y = [0]
     r = [0.5]
-    legende = '\\theta = '+str(theta)
+    
     source_1 = ColumnDataSource(dict(x=x, y=y, r=r, theta=[theta]))
     source_2 = ColumnDataSource(dict(x=[0,np.cos(theta)], y=[0, np.sin(theta)]))
-    source_3 = ColumnDataSource(dict(x=[r[0]*np.cos(theta/2)], y=[r[0]*np.sin(theta/2)], texte=['theta = '+str(theta)]))
+    url = latex_url(theta)
+    source_3 = ColumnDataSource(dict(url=[url],x=[1.5*r[0]*np.cos(theta/2)], y=[1.5*r[0]*np.sin(theta/2)]))
     secteur_arc = Arc(x="x", y="y", radius="r",
                       start_angle=0.0, end_angle="theta",
                       line_color="blue",
@@ -54,7 +109,7 @@ def cercle_trigo_bkh(request: HttpRequest) -> HttpResponse:
                       )
     secteur_line = Line(x="x", y="y",
                         line_color="blue",
-                        line_width=3,
+                        line_width=3
                         )
     secteur_text = Text(x="x", y="y",
                         text_color="blue",
@@ -63,9 +118,11 @@ def cercle_trigo_bkh(request: HttpRequest) -> HttpResponse:
                 
     plot.add_glyph(source_1, secteur_arc)
     plot.add_glyph(source_2, secteur_line)
-    plot.add_glyph(source_3, secteur_text)
+    #plot.add_glyph(source_3, secteur_text)
+    image1 = ImageURL(url="url", x="x", y="y", w=0.45, h=0.1, anchor="left")
+    plot.add_glyph(source_3, image1)
 
-    theta_slider = Slider(start=0., end=np.pi*2, value=theta, step=.01, title="Theta")
+    theta_slider = Slider(start=0., end=np.pi*2, value=theta, step=.1, title="Theta")
     callback = CustomJS(args=dict(source1=source_1, source2=source_2, source3=source_3, theta=theta_slider),
                         code="""
         var csrfToken = '';
@@ -97,7 +154,7 @@ def cercle_trigo_bkh(request: HttpRequest) -> HttpResponse:
             source2.data.y = reponse['s2_y'];
             source3.data.x = reponse['s3_x'];
             source3.data.y = reponse['s3_y'];
-            source3.data.texte = reponse['s3_texte'];
+            source3.data.url = reponse['s3_url'];
             source1.change.emit();
             source2.change.emit();
             source3.change.emit();
@@ -121,13 +178,13 @@ def theta_slider_change(request: HttpRequest) -> HttpResponse:
     x = [0]
     y = [0]
     r = [0.5]
-    
+    url = latex_url(theta)
     #source_1 = ColumnDataSource(dict(x=x, y=y, r=r, theta=[theta]))
     #source_2 = ColumnDataSource(dict(x=[0,np.cos(theta)], y=[0, np.sin(theta)]))
     return JsonResponse(dict(s1_x=x,s1_y=y,s1_r=r,s1_theta=[theta],
                              s2_x=[0,np.cos(theta)], s2_y=[0,np.sin(theta)],
-                             s3_x=[r[0]*np.cos(theta/2)],s3_y=[r[0]*np.sin(theta/2)],
-                             s3_texte=['\\theta = '+str(int(theta*100)/100)]))
+                             s3_x=[1.5*r[0]*np.cos(theta/2)],s3_y=[1.5*r[0]*np.sin(theta/2)],
+                             s3_url=[url]))
     
 def freq_phase(request: HttpRequest) -> HttpResponse:
     freq = 1
@@ -143,7 +200,7 @@ def freq_phase(request: HttpRequest) -> HttpResponse:
     y = np.sin(np.pi*2*freq*x+phase)
     source = ColumnDataSource(data=dict(x=x, y=y))
 
-    plot = figure(y_range=(-1.5, 1.5), width=400, height=400,title="Ma Courbe",name="Mes_donnees")
+    plot = figure(y_range=(-1.5, 1.5), width=1000, height=1000,title="Ma Courbe",name="Mes_donnees")
 
     plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6,name="Mon_sinus")
     plot.add_tools(BoxSelectTool())
