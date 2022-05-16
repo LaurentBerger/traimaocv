3a63708ef13935fdda9695a65e630966e57da284
LaurentBerger <laurent.berger@univ-lemans.fr>
2022-05-16 09:34:31 +0200

    update div

--- bokeh_apps/ts_bokeh.py
+++ bokeh_apps/ts_bokeh.py
@@ -824,7 +824,7 @@ class Fct2D:
             source2.change.emit();
             const div1 = Bokeh.documents[0].get_model_by_name('leg_latex')
             window.alert(div1);
-            div1.text = source4.data.texte
+            div1.text = source4.data.texte[0]
             source4.change.emit();
             }
         xhr.send(dataForm);
